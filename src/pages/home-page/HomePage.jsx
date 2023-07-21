14ff98c5819011aaed99f7f9918f4165501954f5
Arwah <aj.mbarak@gmail.com>
2023-07-21 05:53:21 -0400

    updated homepage styling

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,33 +1,45 @@
-    import { Link } from "react-router-dom";
-    import "./home-page.scss";
-    
-    const HomePage = () => {
-      return (
-        <div className="homepage">
-          <main className="homepage__main">
-            <img className="homepage__logo" alt="logo" src="/" />
-            <h1 className="homepage__header">Meet, Buddy</h1>
-            <p className="homepage__description">Description of Buddy</p>
-            <div className="hompage__link-container">
-              <Link to="/">
-                <p className="homepage__link-no">Not Now</p>
-              </Link>
-              <Link to="/">
-                <p className="homepage__link-yes">Connect</p>
-              </Link>
-            </div>
-    
-            <div className="hompage__privacy">
-              <p className="homepage__privacy-content">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta
-                adipisci voluptas minima aliquam totam ratione laboriosam, a ad
-                neque aspernatur voluptatem ipsum nulla quas, labore non corporis
-                distinctio, excepturi tenetur. 
-              </p>
-            </div>
-          </main>
+import { Link } from "react-router-dom";
+import "./home-page.scss";
+
+const HomePage = () => {
+  return (
+    <main className="homepage">
+      <div className="hompage__logo-container">
+        <img className="homepage__logo" alt="logo" src="/" />
+      </div>
+      <div className="homepage__header-container">
+        <h1 className="homepage__header-top">Your secure AI Health</h1>
+        <h1 className="homepage__header-bottom">Buddy is here</h1>
+      </div>
+      <div className="homepage__bottom-container">
+        <p className="homepage__description">
+          Allow Health Buddy to help you better track your activity goals{" "}
+        </p>
+        <div className="hompage__link-container">
+          <Link to="/" className="homepage__link">
+            <p className="homepage__link-no">Not Now</p>
+          </Link>
+          <Link to="/" className="homepage__link">
+            <p className="homepage__link-yes">Allow Access</p>
+          </Link>
         </div>
-      );
-    };
-    
-    export default HomePage;
+      </div>
+      <div className="hompage__privacy">
+        <p className="homepage__privacy-content">
+          Your use of Google Fit is governed by
+          <p className="homepage__privacy-term-bottom"><Link to="/" className="homepage__privacy-terms">
+            {" "}
+            Google Fit Terms of Service.
+          </Link></p>
+          Note: The{" "}
+          <Link to="/" className="homepage__privacy-terms">
+            Privacy Policy
+          </Link>{" "}
+          describes how data is handled in this service.
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
